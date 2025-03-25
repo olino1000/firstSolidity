77c8195431cf397a6f5abcc2c730f398b7d3ae78
Vinícius Olino <olino1000@gmail.com>
2025-03-25 19:21:20 -0300

    feat: create a compiler and start import the libraries to use

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,4 +1,12 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
+contract Inbox {
+    string public message;
 
-// contract code will go here
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+}
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,6 +1,7 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
+const assert = require('assert');
 
-// updated ganache and web3 imports added for convenience
+const web3 = new Web3(ganache.provider());
 
-// contract test code will go here
+
