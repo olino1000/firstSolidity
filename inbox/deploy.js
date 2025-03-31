85362e70cca3b6d8807788cbb4df5ad03af961e1
Vinícius Olino <olino1000@gmail.com>
2025-03-31 20:56:30 -0300

    feat: Deploy the contract to sepolia test network and interact with him with remix

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,5 +1,27 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require('./compile');
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+    'rubber inflict print ticket razor fly select artefact autumn museum sudden frame',
+    'https://sepolia.infura.io/v3/4747f9aa693343959ca8febbc46403d2'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+}
+
+deploy();
